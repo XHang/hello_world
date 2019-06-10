3cc422a2f453cbef0ed1cdb5c836620c6ee5c9c0
cxh <1083594261@qq.com>
2019-06-10 09:13:03 +0800

    add python notes

--- hello.py
+++ hello.py
@@ -1,8 +1,43 @@
-print(1, 2, 3, 4)
-# Output: 1 2 3 4
+# import moduleA
+# import sys
+#
+# import moduleB
+#
+# print(1, 2, 3, 4)
+# # Output: 1 2 3 4
+#
+# print(1, 2, 3, 4, sep='*')
+# # Output: 1*2*3*4
+#
+# print(1, 2, 3, 4, sep='#', end='&')
+# # Output: 1#2#3#4&
+# print("\r \n")
+# a = 'The value of x is {} and y is {}'.format(1, 2)
+# print(a)
+# print("\r \n")
+# s = '我是{name},今年{age}'.format(name='李大喵', age=13)
+# print(s)
+#
+# # result = input("请输入一个数字:")
+# # print("您输入的参数是:"+result)
+# print(sys.path)
+#
+# print(moduleA.variable)
+# print(moduleB.variable)
 
-print(1, 2, 3, 4, sep='*')
-# Output: 1*2*3*4
 
-print(1, 2, 3, 4, sep='#', end='&')
-# Output: 1#2#3#4&
+def method1():
+    return "fuck ass hole "
+
+
+def method2():
+    return
+
+
+def method3():
+    return 1, 2, 3
+
+
+print(method1())
+print(method2())
+print(method3()[0])
---
+++ operater.py
@@ -0,0 +1,15 @@
+import this
+
+print(this.s)
+
+a = 2
+# Output: id(a) = 10919424
+print('id(a) =', id(a))
+a = a+1
+# Output: id(a) = 10919456
+print('id(a) =', id(a))
+# Output: id(3) = 10919456
+print('id(3) =', id(3))
+b = 2
+# Output: id(2)= 10919424
+print('id(2) =', id(2))
