0c96f8846260e7390dad49cb69819d4ffe19e288
cxh <1083594261@qq.com>
2019-06-11 22:26:17 +0800

    add notes

--- hello.py
+++ hello.py
@@ -1,56 +1,61 @@
-# import moduleA
-# import sys
+import moduleA
+import moduleB
+
+# # import sys
+# #
+# # import moduleB
+# #
+# # print(1, 2, 3, 4)
+# # # Output: 1 2 3 4
+# #
+# # print(1, 2, 3, 4, sep='*')
+# # # Output: 1*2*3*4
+# #
+# # print(1, 2, 3, 4, sep='#', end='&')
+# # # Output: 1#2#3#4&
+# # print("\r \n")
+# # a = 'The value of x is {} and y is {}'.format(1, 2)
+# # print(a)
+# # print("\r \n")
+# # s = '我是{name},今年{age}'.format(name='李大喵', age=13)
+# # print(s)
+# #
+# # # result = input("请输入一个数字:")
+# # # print("您输入的参数是:"+result)
+# # print(sys.path)
+# #
+# # print(moduleA.variable)
+# # print(moduleB.variable)
 #
-# import moduleB
 #
-# print(1, 2, 3, 4)
-# # Output: 1 2 3 4
+# def method1():
+#     return "fuck ass hole "
 #
-# print(1, 2, 3, 4, sep='*')
-# # Output: 1*2*3*4
 #
-# print(1, 2, 3, 4, sep='#', end='&')
-# # Output: 1#2#3#4&
-# print("\r \n")
-# a = 'The value of x is {} and y is {}'.format(1, 2)
-# print(a)
-# print("\r \n")
-# s = '我是{name},今年{age}'.format(name='李大喵', age=13)
-# print(s)
+# def method2():
+#     return
 #
-# # result = input("请输入一个数字:")
-# # print("您输入的参数是:"+result)
-# print(sys.path)
 #
-# print(moduleA.variable)
-# print(moduleB.variable)
+# def method3():
+#     return 1, 2, 3
+#
+#
+# def methon4(name=3):
+#     print("name is [{}]".format(name))
+#
+#
+# methon4(name="safs")
+#
+#
+# def methon5(*names):
+#     for name in names:
+#         print("hello,{}".format(name))
+#
+#
+# methon5("李大喵", "张全碳", "训悟空")
+#
+# double = lambda x: x * 2
+# print(double(2))
+a="asd"
 
-
-def method1():
-    return "fuck ass hole "
-
-
-def method2():
-    return
-
-
-def method3():
-    return 1, 2, 3
-
-
-def methon4(name=3):
-    print("name is [{}]".format(name))
-
-
-methon4(name="safs")
-
-
-def methon5(*names):
-    for name in names:
-        print("hello,{}".format(name))
-
-
-methon5("李大喵", "张全碳", "训悟空")
-
-double = lambda x: x * 2
-print(double(2))
+print(dir(moduleB))
--- moduleA.py
+++ moduleA.py
@@ -1,1 +1,4 @@
+import moduleB
 variable = "模块A里面的变量"
+print("jahhahah")
+moduleB.variable = "你被我修改啦哈哈哈哈哈或"
